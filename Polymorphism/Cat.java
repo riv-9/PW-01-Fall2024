556b3fe3adfa689a5866401a6734447cb51b7a4a
riv-9 <smajumder@fairfield.edu>
2024-11-18 09:15:21 -0500

    Update Cat.java

--- Polymorphism/Cat.java
+++ Polymorphism/Cat.java
@@ -1,14 +1,16 @@
+// Represents a specific type of Animal: Cat
 public class Cat extends Animal {
 
-    static int count;
+    static int count; // Tracks the number of Cat instances created
+
+    // Constructor to initialize a Cat object and increment its count
     public Cat() {
-        super("Cat");
+        super("Cat"); // Calls the superclass (Animal) constructor
         count++;
     }
 
+    // Overridden eat method to specify Cat behavior
     public void eat() {
         System.out.println("Cat is eating");
     }
-
-
 }
