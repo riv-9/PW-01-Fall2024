b383597975de2bfcf50424506d75579f36fd14d1
riv-9 <smajumder@fairfield.edu>
2024-11-18 09:15:52 -0500

    Update Dog.java

--- Polymorphism/Dog.java
+++ Polymorphism/Dog.java
@@ -1,14 +1,16 @@
+// Represents a specific type of Animal: Dog
 public class Dog extends Animal {
 
-    static int count;
+    static int count; // Tracks the number of Dog instances created
+
+    // Constructor to initialize a Dog object and increment its count
     public Dog() {
-        super("Dog");
+        super("Dog"); // Calls the superclass (Animal) constructor
         count++;
     }
 
-    public void eat(){
+    // Overridden eat method to specify Dog behavior
+    public void eat() {
         System.out.println("Dog is eating");
     }
-
-
 }
