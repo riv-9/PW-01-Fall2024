d079f1d2cd4d8bfa43897a147a7f180659d8d092
riv-9 <smajumder@fairfield.edu>
2024-11-18 09:14:46 -0500

    Update AnimalBehavior.java

--- Polymorphism/AnimalBehavior.java
+++ Polymorphism/AnimalBehavior.java
@@ -1,10 +1,16 @@
+// Interface defining common behaviors for all animals
 public interface AnimalBehavior {
 
-    int MAX_AGE = 100;
+    int MAX_AGE = 100; // Maximum age for an animal (a constant)
 
+    // Abstract method to be implemented by classes
     void eat();
-    default void move(){
-        System.out.println("it moves");
-    };
+
+    // Default method for movement, which can be overridden
+    default void move() {
+        System.out.println("It moves");
+    }
+
+    // Abstract method to define animal sounds
     void sound();
 }
