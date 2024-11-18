a3bfd7a3769bb764ebe5ad3249ec15a9e0ff9999
riv-9 <smajumder@fairfield.edu>
2024-11-18 09:14:33 -0500

    Update Animal.java

--- Polymorphism/Animal.java
+++ Polymorphism/Animal.java
@@ -1,23 +1,28 @@
+// Represents a general Animal class implementing the AnimalBehavior interface
 public class Animal implements AnimalBehavior {
 
+    String type; // Stores the type of the animal (e.g., Cat, Dog)
+    static int totalAnimalCount; // Tracks the total number of Animal objects created
+    public static final String KINGDOM = "Animalia"; // A constant representing the biological kingdom
 
-    String type;
-    static int totalAnimalCount;
-    public static final String KINGDOM = "Animalia";
-
+    // Constructor to initialize the type of the animal and increment the total count
     public Animal(String type) {
         this.type = type;
         totalAnimalCount++;
     }
 
-    public void getType(){
+    // Getter method to print the type of the animal
+    public void getType() {
         System.out.println(type);
     }
 
-    public void eat(){
+    // Overridden eat method from the AnimalBehavior interface
+    public void eat() {
         System.out.println("Animal eats");
     }
-    public void sound(){
+
+    // Overridden sound method from the AnimalBehavior interface
+    public void sound() {
         System.out.println("Animal makes sound");
     }
 }
